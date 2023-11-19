5f323b71eb48289b3bf459a3c889b71d1f27e17b
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-11-19 19:04:31 +0600

    change banner

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 import BannerPic from "../../assets/picture/User research-pana.svg";
+import { useNavigate } from "react-router-dom";
 const Banner = () => {
+  const navigate = useNavigate();
   return (
     <div className="mt-20 grid grid-cols-2 gap-5  max-w-7xl mx-auto">
       <div className=" flex items-center">
         <div>
           {" "}
-          <h3 className="text-5xl font-bold">Response Web Design</h3>
+          <h3 className="text-5xl font-bold">Create Team Enjoy Time</h3>
           <div className="h-2 bg-red-500 w-28 mt-3"></div>
           <div className="mt-3">
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Suscipit, veritatis. Neque non eveniet reiciendis repellat! Omnis
-              eos consequuntur quia maxime magnam facere consequatur culpa quam,
-              nostrum provident nihil veniam excepturi.
+              Team Enjoy Time is a dynamic and collaborative group of
+              individuals who share a common passion for creating positive and
+              memorable experiences. Comprising diverse talents and backgrounds,
+              our team is committed to fostering a vibrant and inclusive
+              atmosphere where everyone feels valued and appreciated.
             </p>
             <div className="mt-5">
               {" "}
-              <button className="px-8 py-2 bg-red-500 text-white rounded-xl">
+              <button
+                onClick={() => navigate("/my-team")}
+                className="px-8 py-2 bg-red-500 text-white rounded-xl"
+              >
                 Go Team
               </button>
             </div>
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,1 +1,2 @@
-export const URL = "http://localhost:5000/api/";
+// export const URL = "http://localhost:5000/api/";
+export const URL = "https://user-management-khaki-one.vercel.app/api";
